84527d7ffa51aeec5273f075bb88426517af273a
quocdev1996 <quocdev1996@gmail.com>
2018-11-26 15:04:49 +0700

    change

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/service/user.service';
+
+@Component({
+  selector: 'home-admin',
+  templateUrl: '/home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+  data = new Array();
+  constructor(private userService: UserService) { }
+  ngOnInit() {
+    this.userService.getUser().subscribe((data: any)=>{
+     this.data = data.items || [];
+    })
+  }
+
+}
